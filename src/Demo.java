df8408b8b5a922483314881a5f628ce5fb3b5183
Michael Kastner <michaelkastner@gmx.de>
2017-10-20 15:34:39 +0200

    new: jacSim added

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
@@ -24,7 +26,7 @@ public class Demo {
         // as long as there are lines in the file
 
 
-        ArrayList<ArrayList> set = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> set = new ArrayList<>();
 
         while (line != null) {
             ArrayList<Integer> item = new ArrayList<>();
@@ -46,6 +48,31 @@ public class Demo {
         System.out.println(set);
         System.out.println(threshold);
 
+
+        System.out.println(jacSim(set.get(1), set.get(2)));
+
+    }
+
+    private static <E> double jacSim(ArrayList<E> r, ArrayList<E> s){
+        HashSet<E> intersect = new HashSet<>();
+        HashSet<E> union = new HashSet<>();
+
+        // get union of r and s
+        union.addAll(r);
+        union.addAll(s);
+
+        // get intersect
+        for (E item: union){
+            if (s.contains(item) && r.contains(item))
+                intersect.add(item);
+        }
+
+        return (double)intersect.size()/union.size();
+
+    }
+
+    public static <E, F> boolean verify(ArrayList<E> r, ArrayList<F> M, double t){
+        return false;
     }
 }
 
